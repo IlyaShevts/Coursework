9354ece4409d9d01e9b71707a8344db8c78ed09a
Ilya <soolberns03@gmail.com>
2022-12-25 16:15:44 +0600

    Header

---
+++ app/js/main.js
@@ -0,0 +1,27 @@
+$(function() {
+
+    $('.slider-catalog__inner').slick({
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        prevArrow: '<button class = "slider-blog__item-btn slider-blog__btnprev"><img src = "images/left.svg" alt = "" ></button>',
+        nextArrow: '<button class = "slider-blog__item-btn slider-blog__btnnext"><img src = "images/right.svg" alt = "" ></button>',
+    });
+
+    function tabsResize() {
+        $('.gadgets__item').each(function() {
+            if (window.screen.width >= 768) {
+                $(this).height($(this).width() + 10);
+            } else if (!$(this).hasClass('service')) {
+                $(this).height($(this).width());
+            }
+        });
+    }
+
+    $(document).ready(function() {
+        tabsResize();
+    });
+    $(window).on('scroll resize', function() {
+        tabsResize();
+    });
+});
